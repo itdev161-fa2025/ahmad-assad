589ae949dd49b1e56b28962807860bc42fb50f98
Ahmad Assad <assada5@gmatc.matc.edu>
2024-09-10 18:05:51 -0500

    Set up project

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
