c807d948e85ff65e66bb1343e31c4df8a5f3c8e6
Ahmad Assad <assada5@gmatc.matc.edu>
2024-10-29 13:15:40 -0500

    Updated App.js with navigation and routes, styled header and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
-import Register from './components/Register';
-import Home from './components/Home';
+import { Route, Routes, Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <header className="App-header">
+        <h1>GoodThings</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/register">Register</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/register" element={<div>Register Page</div>} />
+          <Route path="/login" element={<div>Login Page</div>} />
+        </Routes>
+      </main>
     </div>
   );
 }
