f0a7b0afb6e3618ae1edbf25b7ce5f16fe251345
Ahmad Assad <assada5@gmatc.matc.edu>
2024-10-29 13:29:49 -0500

    Updated App.js to use Home, Register, and Login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 import './App.css';
 
 function App() {
